ba1a13ab96e8070b9059960d01fce83dafc2949b
Terry Thompson <terrylthompsonintx@gmail.com>
2020-04-12 17:28:34 -0500

    most of functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,24 +17,29 @@ var pagehead = document.querySelector('#testHead');
 var pagehero = document.querySelector('#testHero');
 var pagefoot = document.querySelector('#testFoot');
 var pagestart = document.querySelector('#testStart');
+var pagecounter = document.querySelector('#timerCount');
+var heroul = document.querySelector('#displayInit');
 var timerDisplay = 0;
 var correctA = 0;
 var a=0;
+var questions =[{q:'What is the color of the sky?',a0:'blue',a1:'green',a2:'yellow',a3:'red',c:'id0'},{q:'What is the color of the grass?',a0:'blue',a1:'green',a2:'yellow',a3:'red',c:'id1'},{q:'What is the color of the banana?',a0:'blue',a1:'green',a2:'yellow',a3:'red',c:'id2'}];
+var qnum =0;
+var initials =[];
 
 //End global variables
 
 var pageloader = function(){
     var heroLabel = document.createElement('H1');
     heroLabel.textContent='Coding Quiz Challenge';
-    pagestart.appendChild(heroLabel);
+    pagehero.appendChild(heroLabel);
     var startText = document.createElement('p');
     startText.textContent='Try to answer code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by tens seconds!';
-    pagestart.appendChild(startText)
+    pagehero.appendChild(startText)
     var startBtn =document.createElement("BUTTON");
     startBtn.textContent= 'Start';
     startBtn.className='startButton';
     startBtn.setAttribute('id','testStartBtn');
-    pagestart.appendChild (startBtn);
+    pagehero.appendChild (startBtn);
 }
 
 var clearDiv =function(targetDiv){
@@ -42,92 +47,223 @@ var clearDiv =function(targetDiv){
     //clears the div passed to it.
 }
 
-var updateTimer =function(secs){
 
-        //updates timer from number passed to funciton
+
+var printQ=function(qpara,ans0, ans1,ans2,ans3){
+    var pagehero = document.querySelector('#testHero');
+    questionMess =document.createElement('p');
+    questionMess.textContent=qpara;
+    pagehero.appendChild(questionMess);
+    var abtn0 = document.createElement('BUTTON');
+    abtn0.textContent=ans0;
+    abtn0.setAttribute('id','id0');
+    abtn0.setAttribute('class','answerbutton')
+    pagehero.appendChild(abtn0);
+    var abtn1 = document.createElement('BUTTON');
+    abtn1.textContent=ans1;
+    abtn1.setAttribute('id','id1');
+    abtn1.setAttribute('class','answerbutton');
+    pagehero.appendChild(abtn1);
+    var abtn2 = document.createElement('BUTTON');
+    abtn2.textContent=ans2;
+    abtn2.setAttribute('id','id2');
+    abtn2.setAttribute('class','answerbutton')
+    pagehero.appendChild(abtn2);
+    var abtn3 = document.createElement('BUTTON');
+    abtn3.textContent=ans3;
+    abtn3.setAttribute('id','id3');
+    abtn3.setAttribute('class','answerbutton')
+    pagehero.appendChild(abtn3);
+}
+
+
+var endTest = function(){
+    clearDiv(pagehero);
+    var dscore = document.createElement('h2');
+    dscore.textContent="Number of correct answers!";
+    pagehero.appendChild(dscore);
+    var hscore = document.createElement('p');
+    hscore.textContent=correctA.toString();
+    pagehero.appendChild(hscore);
+    var userInitstxt = document.createElement('p');
+    userInitstxt.textContent='Enter your intitials'
+    pagehero.appendChild(userInitstxt);
+    var initBox = document.createElement('input');
+    initBox.setAttribute('id','savebox');
+    pagehero.appendChild (initBox);
+
+    var saveBtn =document.createElement("BUTTON");
+    saveBtn.textContent= 'Save';
+    saveBtn.className='saveButton';
+    saveBtn.setAttribute('id','saveBtn');
+    pagehero.appendChild (saveBtn);
+    var promptUse = document.createElement('p');
+    promptUse .textContent='Hit Start to play again.'
+    pagehero.appendChild(promptUse);
+    var startBtn =document.createElement("BUTTON");
+    startBtn.textContent= 'Start';
+    startBtn.className='startButton';
+    startBtn.setAttribute('id','testStartBtn');
+    pagehero.appendChild (startBtn);
+    //Append to localStorage ?
+}
+
+var corA = function(){
+    var answerOut = document.createElement('h2');
+    answerOut.textContent='Correct';
+    pagefoot.appendChild(answerOut);
+    
+}
+
+var wrongA = function(){
+    var answerOut = document.createElement('h2');
+    answerOut.textContent='Incorrect';
+    pagefoot.appendChild(answerOut);
+}
+
+var testq = function(){
+      
+    var clickedBtn= event.target.id;
+    
+    if(clickedBtn==questions[qnum].c){
+        correctA++;
+        corA();
+    }
+    
+    else{
+        wrongA();
+    };
+    
+    clearDiv(pagehero);
+    
+    qnum++;
+    if (qnum == questions.length){
+        endTest()
+    }
+    else {printQ(questions[qnum].q,questions[qnum].a0,questions[qnum].a1,questions[qnum].a2,questions[qnum].a3);
+        
+    };
+}
+
+var saveInitials = function() {
+    var newScore = correctA.toString();
+    var newinits = document.getElementById('savebox').value;
+    var newItem ={
+        initials: newinits,
+        score:newScore
+    }
+    initials.push(newItem);
+
+    localStorage.setItem('userinits',JSON.stringify(initials));
+
+    
+
+   
+
+    
+  }
+var displayInitials = function(){
+    var tempdata =JSON.parse(localStorage.getItem('userinits'));
+    clearDiv(pagehero);
+    if (tempdata != null){
+        
+        initials = tempdata.slice();
+        for (i=0;i<initials.length;i++){
+           var templi= document.createElement('li');
+           templi.textContent = initials[i].initials;
+           console.log (templi);
+           console.log (heroul);
+           heroul.appendChild(templi);
+            
+        }
+    }
+    else {
+        var errormes = document.createElement('h2');
+        errormes.textConent ='No saved initials in localstorage.';
+        pagehero.appendChild(errormes);
+        
+    }
+    var startBtn =document.createElement("BUTTON");
+    startBtn.textContent= 'Start';
+    startBtn.className='startButton';
+    startBtn.setAttribute('id','testStartBtn');
+    pagehero.appendChild (startBtn);
+    
+
 }
 
 
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 // Event handlers
 
-var test = function(){
-    
-    
-    
-    //if event is start button initialize vars. start timer
-    //if event is an answer button, compare answer to correct one.  If correct increment correct answer counter. if not decrement time.  Clear screen load next question.  If no more 
-    
-    var questions =[{q:'What is the color of the sky?',a0:'blue',a1:'green',a2:'yellow',a3:'red',c:'id0'},{q:'What is the color of the grass?',a0:'blue',a1:'green',a2:'yellow',a3:'red',c:'id1'},{q:'What is the color of the banana?',a0:'blue',a1:'green',a2:'yellow',a3:'red',c:'id2'}];
-   
-    clearDiv(pagehero);
-    clearDiv(pagefoot);
 
+var heroEvents= function(){
+    
+    if (event.target.matches("#testStartBtn")) {
         
-        if (a < questions.length){
-            questionMess =document.createElement('p');
-            questionMess.textContent=questions[a].q;
-            pagehero.appendChild(questionMess);
-            var abtn0 = document.createElement('BUTTON');
-            abtn0.textContent=questions[a].a0;
-            abtn0.setAttribute('id','id0');
-            abtn0.setAttribute('class','answerbutton')
-            pagehero.appendChild(abtn0);
-            var abtn1 = document.createElement('BUTTON');
-            abtn1.textContent=questions[a].a1;
-            abtn1.setAttribute('id','id1');
-            abtn1.setAttribute('class','answerbutton')
-            pagehero.appendChild(abtn1);
-            var abtn2 = document.createElement('BUTTON');
-            abtn2.textContent=questions[a].a2;
-            abtn2.setAttribute('id','id2');
-            abtn2.setAttribute('class','answerbutton')
-            pagehero.appendChild(abtn2);
-            var abtn3 = document.createElement('BUTTON');
-            abtn3.textContent=questions[a].a3;
-            abtn3.setAttribute('id','id3');
-            abtn3.setAttribute('class','answerbutton')
-            pagehero.appendChild(abtn3);
-            var correctid =questions[a].c 
-            var answerSelId = event.target.getAttribute('id');
-            console.log(event.target);
-            console.log(correctid);
-            console.log(answerSelId);
-            if (correctid == answerSelId){
-                correctA++;
-                a++;
-                var userOut =  document.createElement('H2');
-                userOut.textContent="Correct";
-                pagefoot.appendChild(userOut);
-                
-            }
-            else if (correctid !== answerSelId && answerSelId !=='testStartBtn'){
-                a++
-                    var userOut =  document.createElement('H2');
-                    userOut.textContent="Incorrect";
-                    pagefoot.appendChild(userOut);
-            }
-        }
-    
-
+        clearDiv(pagehero);
+        qnum=0;
+        correctA=0;
+        setTimeout(endTest,5000);
+        printQ(questions[qnum].q,questions[qnum].a0,questions[qnum].a1,questions[qnum].a2,questions[qnum].a3);
+    }
+   //every time a answerbutton is clicked print a new question.  
+   
+   if (event.target.matches("#id0")){
+       testq();
        
+     
+    };
+   
+   if (event.target.matches("#id1")){
+        testq();
+   };
+   
+   if (event.target.matches("#id2")){
+    testq();
+    };
+   
+    if (event.target.matches("#id3")){
+        testq();
+        };
+   
+   if(event.target.matches("#saveBtn")){
+       saveInitials();
 
-// start timer.
-//iterate thorough questions. 
-    
-    
+   }
 }
 
-var starter = function(){
-    clearDiv(pagestart);
-    setInterval(test(),3000);
+var headEvents= function(){
+    if (event.target.matches("#highScoreBtn")) {
+        displayInitials();
+         
+        
+    }
 }
+
 
 pageloader();
 
-//evant listeners 
 
 
-pagehero.addEventListener('click',test);
-pagestart.addEventListener('click',starter);
+//event listeners 
+pagehero.addEventListener('click',heroEvents);
+pagehead.addEventListener('click',headEvents);
+//pagestart.addEventListener('click',starter);
 
